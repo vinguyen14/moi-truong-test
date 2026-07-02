1364897390e4719d1e7235f73ff8a57d097faa6e
vinguyen14 <vi.nguyenlenhat@gmail.com>
2026-07-02 17:28:57 +0700

    Update codelab-data.js

--- ky-nang/codelab-data.js
+++ ky-nang/codelab-data.js
@@ -1,7 +1,7 @@
 // CẤU HÌNH HỆ THỐNG DÀNH CHO ADMIN
 const APP_CONFIG = {
     // CHỈ DÁN ĐƯỜNG LINK TỪ GOOGLE APPS SCRIPT (CÓ ĐUÔI /exec) VÀO GIỮA 2 DẤU NGOẶC KÉP BÊN DƯỚI:
-    API_URL: "https://script.google.com/macros/s/AKfycbyTvYdGL0aT3xcK4Cwoh8Af9ga8kL7mEAETOLOT8bMRhTlmcoeCNkV0s4L5KvGmoxnCWA/exec", 
+    API_URL: "https://script.google.com/macros/s/AKfycbzrvjEs40TPY-6NresMKH-oWzaOIjCRPPPlUJIfxuwFw1X2bchaWesYPui5wiqRADigjg/exec", 
     COURSE_NAME: "KỸ NĂNG DIRECT SALES" 
 };
 
